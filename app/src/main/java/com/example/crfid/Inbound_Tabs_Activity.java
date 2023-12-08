08590235f52aa8426f61c226e46c49f1e457b01c
Vasant Basutkar <vasantbasutkar143@gmail.com>
2023-12-08 15:35:21 +0530

    Initial commit

---
+++ app/src/main/java/com/example/crfid/Inbound_Tabs_Activity.java
@@ -0,0 +1,14 @@
+package com.example.crfid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Inbound_Tabs_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inbound_tabs);
+    }
+}
