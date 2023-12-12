03def0fe8e13082eec57b231af309f162867a084
MOHNISH9999 <49593401+MOHNISH9999@users.noreply.github.com>
2023-12-12 13:37:56 +0530

    locate assets details page
    -added locate assets details page

---
+++ app/src/main/java/com/example/crfid/Locate_Asset_Details_Page.java
@@ -0,0 +1,16 @@
+package com.example.crfid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public
+class Locate_Asset_Details_Page extends AppCompatActivity {
+
+    @Override
+    protected
+    void onCreate ( Bundle savedInstanceState ) {
+        super.onCreate ( savedInstanceState );
+        setContentView ( R.layout.locate_asset_details_page_activity );
+    }
+}
