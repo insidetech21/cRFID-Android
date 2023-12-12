69d9d250dab25ae69430d0038b5f7bbdb161d421
MOHNISH9999 <49593401+MOHNISH9999@users.noreply.github.com>
2023-12-12 15:57:25 +0530

    basic changes done and errors resolved.

--- app/src/main/java/com/example/crfid/HomePage.java
+++ app/src/main/java/com/example/crfid/HomePage.java
@@ -10,17 +10,27 @@ import android.view.View;
 public class HomePage extends AppCompatActivity {
 
 
-    CardView locateCard;
+    CardView locateCard,assetInOutCard;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
         locateCard=findViewById ( R.id.locateAssets );
+        assetInOutCard=findViewById ( R.id.assetInOut );
         locateCard.setOnClickListener ( new View.OnClickListener ( ) {
             @Override
             public
             void onClick ( View view ) {
                 Intent intent=new Intent( HomePage.this, Locate_Asset.class );
+                startActivity(intent);
+            }
+        } );
+
+        assetInOutCard.setOnClickListener ( new View.OnClickListener ( ) {
+            @Override
+            public
+            void onClick ( View view ) {
+                Intent intent=new Intent( HomePage.this, Material_In_Out.class );
                 startActivity(intent);
             }
         } );
