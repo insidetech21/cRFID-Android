1b4932b804ba8cd67cb74fc603c4e1cb4f2bcee2
MOHNISH9999 <49593401+MOHNISH9999@users.noreply.github.com>
2023-12-12 12:42:20 +0530

    Locate Asset
    -locate asset ui
    -recyclerview common item for locate asset, material tag pair and unpair added.

--- app/src/main/java/com/example/crfid/HomePage.java
+++ app/src/main/java/com/example/crfid/HomePage.java
@@ -1,14 +1,28 @@
 package com.example.crfid;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomePage extends AppCompatActivity {
 
+
+    CardView locateCard;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        locateCard=findViewById ( R.id.locateAssets );
+        locateCard.setOnClickListener ( new View.OnClickListener ( ) {
+            @Override
+            public
+            void onClick ( View view ) {
+                Intent intent=new Intent( HomePage.this, Locate_Asset.class );
+                startActivity(intent);
+            }
+        } );
     }
 }
---
+++ app/src/main/java/com/example/crfid/Locate_Asset.java
@@ -0,0 +1,16 @@
+package com.example.crfid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public
+class Locate_Asset extends AppCompatActivity {
+
+    @Override
+    protected
+    void onCreate ( Bundle savedInstanceState ) {
+        super.onCreate ( savedInstanceState );
+        setContentView ( R.layout.locate_asset_activity );
+    }
+}
