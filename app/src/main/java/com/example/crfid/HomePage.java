5aaa5fa5199e54aa1e4e7fa5c55c5cd3a1aea6da
Vasant Basutkar <vasantbasutkar143@gmail.com>
2023-12-13 16:00:54 +0530

    minor UI Changes

--- app/src/main/java/com/example/crfid/HomePage.java
+++ app/src/main/java/com/example/crfid/HomePage.java
@@ -10,13 +10,14 @@ import android.view.View;
 public class HomePage extends AppCompatActivity {
 
 
-    CardView locateCard,assetInOutCard,matTagPair;
+    CardView locateCard,assetInOutCard,matTagPair,cycleCounting;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
         locateCard=findViewById ( R.id.locateAssets );
         assetInOutCard=findViewById ( R.id.assetInOut );
+        cycleCounting =findViewById(R.id.cycleCounting);
         matTagPair=findViewById ( R.id.assetTagPair );
         locateCard.setOnClickListener ( new View.OnClickListener ( ) {
             @Override
@@ -32,6 +33,15 @@ public class HomePage extends AppCompatActivity {
             public
             void onClick ( View view ) {
                 Intent intent=new Intent( HomePage.this, Material_In_Out.class );
+                startActivity(intent);
+            }
+        } );
+
+        cycleCounting.setOnClickListener ( new View.OnClickListener ( ) {
+            @Override
+            public
+            void onClick ( View view ) {
+                Intent intent=new Intent( HomePage.this, CycleCounting.class );
                 startActivity(intent);
             }
         } );
